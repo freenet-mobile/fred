35bad89b388e29b6ad5dfb0fbb22b13a52081102
DC* <des@riseup.net>
2018-10-11 02:55:53 -0300

    Fix missing gradle.properties

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,12 @@ if [ ! -f "${DOWNLOAD_FULLPATH}" ]; then
     wget "https://bouncycastle.org/download/${BC_PROVIDER_JAR}" -O "${DOWNLOAD_FULLPATH}";
 fi
 cp "${DOWNLOAD_FULLPATH}" "${EXT_FULLPATH}"
+
 ls ${EXT_FULLPATH}
+
 cat /etc/java-7-openjdk/security/java.security|grep security.provider
+
+cp ${HOME}/project/gradle.properties-docker ${HOME}/project/gradle.properties
 cat ${HOME}/project/gradle.properties
 
 gradle jar
